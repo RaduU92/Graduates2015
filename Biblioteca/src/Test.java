e34c42fa302a96cd3429eadaf1d5557ad96e1f5f
RaduU92 <Radu.Ursu@Endava.com>
2015-07-29 16:11:27 +0300

    update

--- Biblioteca/src/Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by rursu on 7/20/2015.
- */
-public class Test {
-    public void afisare(Carte c) {
-        System.out.println("Autor: " + c.autor + ", \"" + c.titlu + "\", Editura: " + c.editura + ", numar pagini: " + c.numarPagini);
-    }
-}
