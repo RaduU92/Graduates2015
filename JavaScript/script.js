0353c9a97a7d0b36e06db7e5337b11080ff34f7a
RaduU92 <Radu.Ursu@Endava.com>
2015-08-10 20:23:52 +0300

    10.08.2015

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,5 @@
-console.log("In external file");
+function checkFirstName() {
+	console.log("campul prenume este completat cu: " + document.getElementById('firstName').value);
+	console.log("campul nume este completat cu: " + document.getElementById('lastName').value);
+	console.log("campul data nasterii este completat cu: " + document.getElementById('birthDate').value);
+}
